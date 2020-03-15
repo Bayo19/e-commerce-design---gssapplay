61337dbc53beebd6a6a30dfe270c6b4438e7a5a0
Bayo Ade <bayo.300@gmail.com>
2020-03-15 09:35:01 +0000

    little change to animation delay duration

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ TweenMax.from(".pic-2", 1, {
 })
 
 TweenMax.from(".pic-1", 1, {
-    delay: 0.8,
+    delay: 0.,
     x: -10,
     opacity: 0,
     ease: Expo.easeInOut
 })
 
 TweenMax.from(".pic1title", 0.95, {
-    delay: 1.1,
+    delay: 0.99,
     y: 7,
     x: 6,
     opacity: 0,
     ease: Expo.easeIn
 })
 TweenMax.from(".pic2title", 0.95, {
-    delay: 1.1,
+    delay: 1.01,
     y: 7,
     x: 6,
     opacity: 0,
